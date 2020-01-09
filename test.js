2beed7e83f593c95b0bfce8badd08fc3029e9bd1
Thirumalai, Vijayakumar X <dbakumar@gmail.com>
2020-01-09 12:02:51 -0600

    added test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+QUnit.test( "hello test", function( assert ) {
+    assert.ok( 1 == "2", "Passed!" );
+});
+
+QUnit.test( "hello test2", function( assert ) {
+    assert.ok( 1 == "3", "Passed!" );
+});
